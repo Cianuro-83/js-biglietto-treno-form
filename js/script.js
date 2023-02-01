d41c37fd2cb2c2d7833ed5bddcfc1e7ebf09fa57
Cianuro-83 <buzzeosimone@yahoo.com>
2023-02-01 23:53:19 +0100

    FEAT: ALL JS SCRIPT

--- js/script.js
+++ js/script.js
@@ -3,45 +3,83 @@ console.log("TEST");
 const kmElement = document.querySelector("#km");
 const ageElement = document.querySelector("#eta");
 const btnElement = document.querySelector("#btn");
-console.log(btnElement, ageElement, kmElement);
+let userNameElement = document.getElementById("nome-utente");
+let surnameElement = document.getElementById("cognome-utente");
+let firstNameElement = document.getElementById("first-name");
+let lastNameElement = document.getElementById("last-name");
+console.log(
+  btnElement,
+  ageElement,
+  kmElement,
+  firstNameElement,
+  lastNameElement,
+  userNameElement,
+  surnameElement
+);
 
 // creo la variabile del prezzo al km
 const prize = 0.21;
 
-// creo le variabili del prezzo
-let fullPrize = prize * km,
-  youngPrize = 0,
-  oldPrize = 0,
-  youngDiscount = 0.2,
-  olderDiscount = 0.4;
-
 // impostol'evento al click del mouse
 btnElement.addEventListener("click", function () {
   console.log("Click");
+  // creo le variabili del prezzo
+  let youngPrize,
+    oldPrize,
+    fullPrize,
+    youngDiscount,
+    oldDiscount,
+    youngDiscountPercent = 0.2,
+    oldDiscountPercent = 0.4,
+    itCost,
+    name = firstNameElement.value;
+  surname = lastNameElement.value;
   // creo la variabile relativa all'età
   let age = ageElement.value;
   // creo la variabile relativa ai km
-  let km = parseFloat(kmElement.value);
-  console.log(age, km);
-  //assegno valori all'età e calcolo lo sconto
-  switch (ageElement) {
-    case "minorenne":
-      age < "17";
-      youngPrize = fullPrize * youngDiscount;
+  let km = parseInt(kmElement.value);
+  console.log(age, "km", km);
 
+  fullPrize = prize * km;
+  // calcolo il prezzo dei biglietti per minorenne
+  youngDiscount = [fullPrize * youngDiscountPercent - fullPrize] * -1;
+  youngPrize = youngDiscount.toFixed(2);
+  // calcolo il prezzo dei biglietti per anziano
+  oldDiscount = [fullPrize * oldDiscountPercent - fullPrize] * -1;
+  oldPrize = oldDiscount.toFixed(2);
+  console.log(fullPrize, youngPrize, oldPrize);
+
+  //   if (age === "minorenne") {
+  //     itCost = youngPrize;
+  //     console.log(itCost);
+  //   } else if (age === "adulto") {
+  //     itCost = fullPrize;
+  //     console.log(ItCost);
+  //   } else {
+  //     itCost = oldPrize;
+  //     console.log(itCost);
+  //   }
+
+  switch (age) {
+    case "minorenne":
+      itCost = youngPrize;
+      console.log(itCost);
       break;
     case "adulto":
-      age >= "18" && age <= "64";
-      fullPrize;
-
+      itCost = fullPrize.toFixed(2);
+      console.log(itCost);
       break;
     case "anziano":
-      age >= "65";
-      oldPrize = full * olderDiscount;
-    //   break;
-    // default:
-    //   age = 39;
+      itCost = oldPrize;
+      console.log(itCost);
+      break;
+    default:
+      itCost = fullPrize.toFixed(2);
+      console.log(itCost);
   }
 
-  console.log(fullPrize, youngPrize, oldPrize);
+  let toPayElement = document.getElementById("costo-biglietto");
+  toPayElement.innerHTML = itCost;
+  userNameElement.innerHTML = name;
+  surnameElement.innerHTML = surname;
 });
