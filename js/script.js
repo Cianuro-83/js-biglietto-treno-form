562b599da8eb50a5b3f749355df7c646e58e8911
Cianuro-83 <buzzeosimone@yahoo.com>
2023-02-01 15:59:33 +0100

    FEAT:associato bottone al click

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 console.log("TEST");
+// assegno una variabile agli elementi html
 const kmElement = document.querySelector("#km");
 const ageElement = document.querySelector("#eta");
 const btnElement = document.querySelector("#btn");
 console.log(btnElement, ageElement, kmElement);
+
+// creo la variabile del prezzo al km
+const prize = 0.21;
+
+// creo le variabili del prezzo
+let fullPrize, youngPrize, olderPrize;
+
+// impostol'evento al click del mouse
+btnElement.addEventListener("click", function () {
+  console.log("Click");
+});
