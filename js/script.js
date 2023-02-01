bef42fd1eb7dfb848541c29e78decce267b64830
Cianuro-83 <buzzeosimone@yahoo.com>
2023-02-01 15:34:14 +0100

    FEAT: impostazione base esercizio

---
+++ js/script.js
@@ -0,0 +1,5 @@
+console.log("TEST");
+const kmElement = document.querySelector("#km");
+const ageElement = document.querySelector("#eta");
+const btnElement = document.querySelector("#btn");
+console.log(btnElement, ageElement, kmElement);
