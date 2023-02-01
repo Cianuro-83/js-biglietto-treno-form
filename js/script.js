e57bcf4db88b45c5c52762305aafde7d72eb9993
Cianuro-83 <buzzeosimone@yahoo.com>
2023-02-01 17:00:18 +0100

    try to calculate discount

--- js/script.js
+++ js/script.js
@@ -9,9 +9,39 @@ console.log(btnElement, ageElement, kmElement);
 const prize = 0.21;
 
 // creo le variabili del prezzo
-let fullPrize, youngPrize, olderPrize;
+let fullPrize = prize * km,
+  youngPrize = 0,
+  oldPrize = 0,
+  youngDiscount = 0.2,
+  olderDiscount = 0.4;
 
 // impostol'evento al click del mouse
 btnElement.addEventListener("click", function () {
   console.log("Click");
+  // creo la variabile relativa all'età
+  let age = ageElement.value;
+  // creo la variabile relativa ai km
+  let km = parseFloat(kmElement.value);
+  console.log(age, km);
+  //assegno valori all'età e calcolo lo sconto
+  switch (ageElement) {
+    case "minorenne":
+      age < "17";
+      youngPrize = fullPrize * youngDiscount;
+
+      break;
+    case "adulto":
+      age >= "18" && age <= "64";
+      fullPrize;
+
+      break;
+    case "anziano":
+      age >= "65";
+      oldPrize = full * olderDiscount;
+    //   break;
+    // default:
+    //   age = 39;
+  }
+
+  console.log(fullPrize, youngPrize, oldPrize);
 });
